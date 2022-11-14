d698d09c5a7ddfe3524a933020933988682feab1
marcoaufiero <marcoaufiero96@gmail.com>
2022-11-14 14:40:28 +0100

    input and button added on HTML

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+  
+        todoList: [
+            {
+                text: 'Comprare la farina',
+                done: false
+            },
+            {
+                text: 'Aggiustare la mensola in cucina',
+                done: false
+            }
+        ],
+
+        todoCompleted: []
+    },
+
+    methods:{
+        
+        
+    }
+})
